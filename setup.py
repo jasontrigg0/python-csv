2f4a4fa63219e6af35baa6593d77d450b063c074
jasontrigg0 <jasontrigg0@gmail.com>
2015-12-27 00:36:03 -0500

    pypi version 0.0.2

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from distutils.core import setup
 setup(
     name = 'python-csv',
-    packages = ['python-csv'],
-    version = "0.0.2",
+    packages = ['python_csv'],
+    version = "0.0.1",
     description = 'Python tools for manipulating csv files',
     author = "Jason Trigg",
     author_email = "jasontrigg0@gmail.com",
     url = "https://github.com/jasontrigg0/python-csv",
-    download_url = 'https://github.com/jasontrigg0/python-csv/tarball/0.0.2',
+    download_url = 'https://github.com/jasontrigg0/python-csv/tarball/0.0.1',
     scripts=[
-        "python-csv/pcsv",
-        "python-csv/pagg",
-        "python-csv/pgraph",
-        "python-csv/pjoin",
-        "python-csv/plook",
-        "python-csv/psort",
-        "python-csv/pset",
-        "python-csv/ptable",
-        "python-csv/to_csv"
+        "python_csv/pcsv",
+        "python_csv/pagg",
+        "python_csv/pgraph",
+        "python_csv/pjoin",
+        "python_csv/plook",
+        "python_csv/psort",
+        "python_csv/pset",
+        "python_csv/ptable",
+        "python_csv/to_csv"
     ],
     install_requires=[
         "numpy",
